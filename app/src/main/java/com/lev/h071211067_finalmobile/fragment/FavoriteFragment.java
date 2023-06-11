e84b08d2952f58ac9afc6d70de5c6a067f8263ea
Adhitya Fajar Risqi Djati Pratama <levafr369@gmail.com>
2023-06-11 22:04:38 +0800

    Feat: Favorite Fragment
    Menambahkan Favorite Fragment

--- app/src/main/java/com/lev/h071211067_finalmobile/fragment/FavoriteFragment.java
+++ app/src/main/java/com/lev/h071211067_finalmobile/fragment/FavoriteFragment.java
@@ -1,6 +1,34 @@
 package com.lev.h071211067_finalmobile.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lev.h071211067_finalmobile.R;
+import com.lev.h071211067_finalmobile.recyclerview.FavoriteAdapter;
 
 public class FavoriteFragment extends Fragment {
+    private RecyclerView rvFavorites;
+    private FavoriteAdapter favoriteAdapter;
+
+    public FavoriteFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        rvFavorites = view.findViewById(R.id.rvFavorite);
+        rvFavorites.setLayoutManager(new LinearLayoutManager(getContext()));
+        favoriteAdapter = new FavoriteAdapter();
+        rvFavorites.setAdapter(favoriteAdapter);
+        return view;
+    }
 }
--- app/src/main/java/com/lev/h071211067_finalmobile/recyclerview/FavoriteAdapter.java
+++ app/src/main/java/com/lev/h071211067_finalmobile/recyclerview/FavoriteAdapter.java
@@ -1,31 +1,103 @@
 package com.lev.h071211067_finalmobile.recyclerview;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.lev.h071211067_finalmobile.DetailActivity;
+import com.lev.h071211067_finalmobile.R;
+import com.lev.h071211067_finalmobile.networking.Favorite;
+import com.lev.h071211067_finalmobile.networking.Movie;
+import com.lev.h071211067_finalmobile.networking.TVShow;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder> {
+    private List<Favorite> favorites;
+
+    public FavoriteAdapter() {
+        this.favorites = new ArrayList<>();
+    }
+
+    public void setFavorites(List<Favorite> favorites) {
+        this.favorites = favorites;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
-    public FavoriteAdapter.FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_favorite, parent, false);
+        return new FavoriteViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FavoriteAdapter.FavoriteViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull FavoriteViewHolder holder, int position) {
+        Context context = holder.itemView.getContext();
+        Favorite favorite = favorites.get(position);
+        holder.setData(favorite, context);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return favorites.size();
     }
 
-    public class FavoriteViewHolder extends RecyclerView.ViewHolder {
+    static class FavoriteViewHolder extends RecyclerView.ViewHolder {
+        private ImageView posterImageView;
+        private ImageView typeImageView;
+        private TextView titleTextView;
+        private TextView tanggalTextView;
+
         public FavoriteViewHolder(@NonNull View itemView) {
             super(itemView);
+            typeImageView = itemView.findViewById(R.id.iv_type);
+            posterImageView = itemView.findViewById(R.id.iv_poster);
+            titleTextView = itemView.findViewById(R.id.tv_title);
+            tanggalTextView = itemView.findViewById(R.id.tvRilisFavorite);
+        }
+
+        public void setData(Favorite favorite, Context context) {
+            String title;
+            String poster;
+
+            if (favorite.isMovie()) {
+                Toast.makeText(context, "AAAA", Toast.LENGTH_SHORT).show();
+                Movie movie = favorite.getMovie();
+                title = movie.getTitle();
+                poster = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + movie.getPosterPath();
+                typeImageView.setImageResource(R.drawable.round_movie_24);
+            } else {
+                Toast.makeText(context, "BBBBB", Toast.LENGTH_SHORT).show();
+                typeImageView.setImageResource(R.drawable.round_tv_24);
+                TVShow tvShow = favorite.getTvShow();
+                title = tvShow.getName();
+                poster = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + tvShow.getPosterPath();
+            }
+
+            titleTextView.setText(title);
+
+            Glide.with(context)
+                    .load(poster)
+                    .into(posterImageView);
+
+            itemView.setOnClickListener(view -> {
+                Intent intent = new Intent(itemView.getContext(), DetailActivity.class);
+                intent.putExtra("favorite", favorite);
+                itemView.getContext().startActivity(intent);
+            });
+
         }
     }
 }
